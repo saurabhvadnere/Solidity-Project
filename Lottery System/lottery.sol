fcb285c183f4f10a6510c6b27d725bd96ee19978
saurabhvadnere <102731958+saurabhvadnere@users.noreply.github.com>
2025-12-13 23:59:07 +0530

    Lottery.sol

--- Lottery System/lottery.sol
+++ Lottery System/lottery.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.0;
 
 contract Lottery
 {
@@ -41,3 +41,4 @@ contract Lottery
        participants = new address payable[](0);
     }
 }
+
